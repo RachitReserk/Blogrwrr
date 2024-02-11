86fe04d7c10926971195d7d8d2f7e18084eb91e0
RachitReserk <trachit752@gmail.com>
2024-02-11 12:26:24 +0530

    stage I prep 1

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const blogSchema = new mongoose.Schema({
 const Blog = mongoose.model('Blog',blogSchema)
 
 const mongoURL = 'mongodb+srv://trachit752:rachit123@cluster0.aj5o1f1.mongodb.net/blogs?retryWrites=true&w=majority'
+mongoose.connect(mongoURL).then(res => {
+    console.log("Connected")
+})
 
 app.use(cors())
 app.use(express.json())
@@ -25,8 +28,8 @@ app.get('/api/blogs',(request,response) => {
 
 app.post('/api/blogs',(request,response) => {
     const blog = new Blog(request.body)
-
     blog.save().then(result => {
+        console.log(result)
         response.status(201).json(result)
     })
 })
